d8d16bdfb6bb1699f4b295381b0e1356d19e3fc3
vitoko1 <38541062+vitoko1@users.noreply.github.com>
2022-05-18 20:30:16 -0700

    add new changes couintries (filter)

--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -13,6 +13,8 @@ export default class extends AbstractView {
     async getHTML() {
         // console.log("FILTER TEST BELOW");
         // console.log(filterFunction("region", "Americas"))
+
+        createFilterSection();
         return "<h1>Countries Victor</h1>";
 
     }
@@ -20,6 +22,77 @@ export default class extends AbstractView {
 
 }
 
+const createFilterSection = function (){
+ 
+    let newELement= document.createElement('section')
+
+    newELement.className = 'home';
+
+    let divInFlex = document.createElement('div');
+
+    divInFlex.className = 'in-flex';
+
+    let newInput = document.createElement('input');
+
+    newInput.className= 'text';
+    newInput.label='label';
+
+    let newLabel =  document.createElement('label');
+
+    newLabel.innerHTML= 'Select Filter:';
+
+    let newSelect = document.createElement('select');
+
+    newSelect.id = 'filterSelect';
+
+    let option1= document.createElement('option');
+    option1.value='region';
+    option1.label='Region';
+    let option2= document.createElement('option');
+    option2.value='name';
+    option2.label='Name';
+
+    let buttonF =document.createElement('button');
+
+    buttonF.id='filterButton';
+    buttonF.textContent='Filter';
+
+
+    newSelect.appendChild(option1);
+    newSelect.appendChild(option2);
+
+    divInFlex.appendChild(newInput);
+    divInFlex.appendChild(newLabel);
+    divInFlex.appendChild(newSelect);
+    divInFlex.appendChild(buttonF);
+
+    //let container = document.getElementById('test10');
+    //container.style.visibility = "hidden";
+
+    let blancDiv = document.createElement('div');
+    let divFlags = document.createElement('div');
+
+    divFlags.className= 'flags';
+
+    let img = document.createElement('img');
+    img.src="/design/chile.png";
+    img.alt='chile';
+
+
+
+
+    blancDiv.appendChild(divFlags);
+    divInFlex.appendChild(blancDiv);
+
+
+    newELement.appendChild(divInFlex);
+
+    //container.appendChild(newELement);
+    document.body.appendChild(newELement)
+
+
+
+}
 
 const filterFunction = function (filterBy, input) {
     let filteredCountries = [];
