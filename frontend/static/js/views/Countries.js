b8e8b05f9662918fc7812e858ef3b6719abec574
eacamposh <eacamposh@gmail.com>
2022-05-20 01:21:40 -0700

    update flags

--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -158,55 +158,51 @@ const filterFunction = function (filterBy, input) {
     document.body.appendChild(divToBody);
 
     let newELement = document.createElement('section')
-
     newELement.className = 'home';
 
     let divInFlex = document.createElement('div');
-
     divInFlex.className = 'in-flex';
 
     let newInput = document.createElement('input');
-
     newInput.id = 'inputId';
 
     newInput.className = 'text';
     newInput.label = 'label';
 
     let newLabel = document.createElement('label');
-
     newLabel.innerHTML = 'Select Filter:';
 
     let newSelect = document.createElement('select');
-
     newSelect.id = 'filterSelect';
 
     let option1 = document.createElement('option');
     option1.value = 'region';
     option1.label = 'Region';
+
     let option2 = document.createElement('option');
     option2.value = 'name';
     option2.label = 'Name';
 
     let buttonF = document.createElement('button');
-
     buttonF.id = 'filterButton';
     buttonF.textContent = 'Filter';
 
 
-
     newSelect.appendChild(option1);
     newSelect.appendChild(option2);
+
 
     divInFlex.appendChild(newInput);
     divInFlex.appendChild(newLabel);
 
     divInFlex.appendChild(newSelect);
     divInFlex.appendChild(buttonF);
+    divInFlex.appendChild(blancDiv);
+
 
     newELement.appendChild(divInFlex);
     divToBody.appendChild(newELement);
 
-    divToBody.appendChild(blancDiv);
     document.body.appendChild(divToBody);
 
 
